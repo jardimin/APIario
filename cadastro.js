ce91cdc7bc1887c94440ac6d2b20035ddcbb3b39
root <root@colmeia.aovivonaweb.tv>
2015-06-15 21:24:15 +0000

    Ajuste de cadastro

--- cadastro.js
+++ cadastro.js
@@ -7,15 +7,15 @@ hashPassword = function(password) {
   var salt = bcrypt.genSaltSync(10);
   return bcrypt.hashSync(password, salt);
 }
-//Criação dé um usuário com sua página de retorno, a página de retorno sempre será verificada
+//Criação do um usuário com sua página de retorno, a página de retorno sempre será verificada
 models.User.create({
-  email: 'suporte@haarieh.com',
-  hashed_password: hashPassword('Beavis')
+  email: 'suporte@haarieh.com', //Login do usuário
+  hashed_password: hashPassword('Beavis') //Senha do usuário
 }, function() {
   models.OAuthClientsModel.create({
-    clientId: '28471984739287473',
-    clientSecret: '3947839820',
-    redirectUri: 'http://localhost/teste/index.php'
+    clientId: '28471984739287473', //Mantenha o mesmo clientID
+    clientSecret: '3947839820', //Pode ser alterado dependendo do cliente
+    redirectUri: 'http://localhost/teste/index.php' //URL de retorno que também vai ser usado para informar que o vídeo está pronto
   }, function() {
     process.exit();
   });
